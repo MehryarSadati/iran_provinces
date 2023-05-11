2e1fe09f03945d7917179b3656963fdc9e2018ea
Mehryar Sadati <mehryarsadati@gmail.com>
2023-05-12 02:34:52 +0330

    reformat with black

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = turtle.Screen()
 screen.title("Iran states game")
 image = "iran_map.gif"
 screen.addshape(image)
-screen.setup(width= 1200, height= 1071)
+screen.setup(width=1200, height=1071)
 turtle.shape(image)
 
 t = turtle.Turtle()
@@ -13,13 +13,13 @@ t.penup()
 t.hideturtle()
 
 persian_data = pandas.read_csv("ostans.csv")
-# x_axis = data.x.to_list()
-# y_axis = data.y.to_list()
 
 english_data = pandas.read_csv("english_ostans.csv")
-# english_ostans = english_data.ostan.to_list()
 
-language = screen.textinput(title= "language", prompt="would you rather english names or persian names?(english/persian)")
+language = screen.textinput(
+    title="language",
+    prompt="would you rather english names or persian names?(english/persian)",
+)
 if language == "english":
     data = english_data
 else:
@@ -30,7 +30,9 @@ ostans = data.ostan.to_list()
 right_answers = 0
 is_game_on = True
 while is_game_on:
-    answer = screen.textinput(title= f"{right_answers}/31 ostans correct", prompt= "name a ostan in iran:").lower()
+    answer = screen.textinput(
+        title=f"{right_answers}/31 ostans correct", prompt="name a ostan in iran:"
+    ).lower()
     if answer == "exit":
         is_game_on = False
 
@@ -42,8 +44,8 @@ while is_game_on:
             t.write(ostan)
             ostans.remove(ostan)
     if right_answers == 31:
-        t.goto(0,0)
-        t.write("CONGRATULATIONS",align= "center", font=("Arial", 50,"bold"))
+        t.goto(0, 0)
+        t.write("CONGRATULATIONS", align="center", font=("Arial", 50, "bold"))
         is_game_on = False
 
 un_guessed_states = pandas.DataFrame(ostans)
